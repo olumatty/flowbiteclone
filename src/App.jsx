be4635c94e9c9721746caa2395b03e81ddda6725
Matthew Adebayo <olumatty@gmail.com>
2025-02-25 16:30:55 +0100

    Header 5.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ThemeProvider } from "./ThemeContext";
 import Header from "./pages/Global/Header";
 import Home from "./pages/Home/Index";
 import Footer from "./pages/Global/Footer";
@@ -12,13 +11,27 @@ import Blocks from "./pages/Blocks/Index";
 import ProVersion from "./pages/ProVersion/Index";
 import Pricing from "./pages/Pricing/Index";
 import Login from "./pages/Login/Index";
+import {useState, useEffect} from "react";
 
 const App = () => {
+  const [darkMode, setDarkMode] = useState(()=> localStorage.getItem("theme")==="dark");
+
+  useEffect(() => {
+    const root = document.documentElement; 
+    if (darkMode) {
+      root.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      root.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+    console.log("Current Theme:", darkMode ? "Dark" : "Light");  // Debugging
+  }, [darkMode]);
+
   return (
-    <ThemeProvider>
-      <div className="flex flex-col min-h-screen bg-white dark:bg-gray-900 text-black dark:text-white">
+      <div className={`flex flex-col min-h-screen transition-all duration-200 ${darkMode ? "bg-gray-900 text-white" : "bg-white text-gray-900"}`}>
         <BrowserRouter>
-          <Header />
+          <Header key={darkMode ? "dark" : "light"} darkMode={darkMode} setDarkMode={setDarkMode}/>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/blocks" element={<Blocks />} />
@@ -33,7 +46,6 @@ const App = () => {
           <Footer />
         </BrowserRouter>
       </div>
-    </ThemeProvider>
   );
 };
 
--- src/pages/Global/Header.jsx
+++ src/pages/Global/Header.jsx
@@ -2,29 +2,32 @@ import React, { useContext } from "react";
 import Logo from "../../assets/Logo.svg";
 import { RiSearch2Line } from "react-icons/ri";
 import { FaGithub, FaDiscord, FaYoutube } from "react-icons/fa";
-import { MdDarkMode, MdLightMode, MdMenu, MdArrowRightAlt } from "react-icons/md";
-import { ThemeContext } from "../../ThemeContext";
+import {
+  MdDarkMode,
+  MdLightMode,
+  MdMenu,
+  MdArrowRightAlt,
+} from "react-icons/md";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 
-const Header = () => {
-  const { darkMode, handleToggle } = useContext(ThemeContext);
+const Header = ({darkMode, setDarkMode}) => {
   const [isOpen, setIsOpen] = useState(false);
   const navigate = useNavigate();
 
   return (
     <div className="sticky top-0 z-40 flex-none mx-auto w-full max-w-[1440px]">
-      <header className="sticky w-full px-3 py-3 mx-auto fixed left-0 top-0 z-30">
-        <div className="hidden lg:flex items-center lg:justify-between">
+      <header className="w-full px-3 py-3 fixed top-0 z-30 max-w-[1440px] mx-auto bg-whit">
+        <div className="hidden  lg:flex items-center lg:justify-between">
           <div className="flex items-center gap-x-12">
             <div className="flex items-center gap-x-3">
-              <img src={Logo} alt="logo" className="max-w-full" />
+              <img src={Logo} alt="logo" className="" />
               <span className="self-center text-2xl font-semibold whitespace-nowrap">
                 Flowbite
               </span>
             </div>
 
-            <div className="flex items-center justify-between rounded-[8px] px-3 w-[300px] h-10 dark:bg-gray-700 text-gray-400 cursor-pointer hover:dark:bg-gray-500">
+            <div className={`flex z-50 items-center justify-between rounded-[8px] px-3 min-w-[300px] h-10  cursor-pointer ${darkMode?  "dark:bg-gray-700 dark:text-gray-400 hover:dark:bg-gray-500" : "bg-gray-50 border text-gray-400 border-gray-200"}`}>
               <div className="flex items-center gap-2">
                 <RiSearch2Line />
                 <span className="text-[14px] font-medium">Search</span>
@@ -38,34 +41,77 @@ const Header = () => {
           </div>
 
           <div className="gap-6 flex items-center">
-            <ul className="flex items-center gap-4 dark:text-gray-300 cursor-pointer">
-              <li onClick={() => navigate("/blocks")} className="hover:dark:text-blue-700">Blocks</li>
-              <li onClick={() => navigate("/figma")} className="hover:dark:text-blue-700">Figma</li>
-              <li onClick={() => navigate("/icons")} className="hover:dark:text-blue-700">Icons</li>
-              <li onClick={() => navigate("/illustrations")} className="hover:dark:text-blue-700">Illustrations</li>
-              <li onClick={() => navigate("/blog")} className="hover:dark:text-blue-700">Blog</li>
-              <li onClick={() => navigate("/proversion")} className="hover:dark:text-blue-700">Pro version</li>
-              <li onClick={() => navigate("/pricing")} className="hover:dark:text-blue-700">Pricing & FAQ</li>
+            <ul className={`flex items-center gap-4 ${ darkMode ? "dark:text-gray-300" : "text-black "} cursor-pointer`}>
+              <li
+                onClick={() => navigate("/blocks")}
+                className="hover:dark:text-blue-700"
+              >
+                Blocks
+              </li>
+              <li
+                onClick={() => navigate("/figma")}
+                className="hover:dark:text-blue-700"
+              >
+                Figma
+              </li>
+              <li
+                onClick={() => navigate("/icons")}
+                className="hover:dark:text-blue-700"
+              >
+                Icons
+              </li>
+              <li
+                onClick={() => navigate("/illustrations")}
+                className="hover:dark:text-blue-700"
+              >
+                Illustrations
+              </li>
+              <li
+                onClick={() => navigate("/blog")}
+                className="hover:dark:text-blue-700"
+              >
+                Blog
+              </li>
+              <li
+                onClick={() => navigate("/proversion")}
+                className="hover:dark:text-blue-700"
+              >
+                Pro version
+              </li>
+              <li
+                onClick={() => navigate("/pricing")}
+                className="hover:dark:text-blue-700"
+              >
+                Pricing & FAQ
+              </li>
             </ul>
 
-            <div className="flex items-center gap-2 cursor-pointer dark:text-gray-300">
-              <div className="p-2 rounded-[4px] hover:bg-gray-700 hover:text-white transition-all duration-200">
+            <div className={`flex items-center gap-2 cursor-pointer ${darkMode ?"dark:text-gray-400" : "text-gray-500"}`}>
+              <div className={`p-2 rounded-[4px] inline-flex ${darkMode ? "hover:bg-gray-700 hover:text-white transition-all duration-200 focus:outline-none focus:ring-4 dark:focus:ring-gray-700 "
+              :"focus:outline-none focus:ring-4 focus:ring-gray-200"}`}>
                 <FaGithub className="text-[16px]" />
               </div>
-              <div className="p-2 rounded-[4px] hover:bg-gray-700 hover:text-white transition-all duration-200">
+              <div className={`p-2 rounded-[4px] inline-flex ${darkMode ? "hover:bg-gray-700 hover:text-white transition-all duration-200 focus:outline-none focus:ring-4 dark:focus:ring-gray-700 "
+              :"focus:outline-none focus:ring-4 focus:ring-gray-200"}`}>
                 <FaDiscord className="text-[16px]" />
               </div>
-              <div className="p-2 rounded-[4px] hover:bg-gray-700 hover:text-white transition-all duration-200">
+              <div className={`p-2 rounded-[4px] inline-flex ${darkMode ? "hover:bg-gray-700 hover:text-white transition-all duration-200 focus:outline-none focus:ring-4 dark:focus:ring-gray-700 "
+              :"focus:outline-none focus:ring-4 focus:ring-gray-200"}`}>
                 <FaYoutube className="text-[16px]" />
               </div>
-              <button onClick={handleToggle} className="p-2 rounded-[4px] hover:bg-gray-700 hover:text-white transition-all duration-200">
-                {darkMode ? <MdDarkMode /> : <MdLightMode />}
+              <button
+                onClick={() => setDarkMode(!darkMode)}
+                className={`p-2 rounded-[4px] inline-flex ${darkMode ? "hover:bg-gray-700 hover:text-white transition-all duration-200 focus:outline-none focus:ring-4 dark:focus:ring-gray-700 "
+                  :"focus:outline-none focus:ring-4 focus:ring-gray-200"}`}>
+                {darkMode ? <MdDarkMode />: <MdLightMode />}
               </button>
             </div>
 
             <div className="flex">
-              <button onClick={() => navigate("/login")}
-                className="py-[10px] px-4 rounded-[10px] bg-blue-700 text-white cursor-pointer flex items-center transition-all duration-200 ease-linear group hover:bg-blue-900">
+              <button
+                onClick={() => navigate("/login")}
+                className="py-[10px] px-4 rounded-[10px] bg-blue-700 text-white cursor-pointer flex items-center transition-all duration-200 ease-linear group hover:bg-blue-900"
+              >
                 Sign in
                 <MdArrowRightAlt className="w-5 h-5 ml-1 transition-all duration-200 ease-in-out group-hover:translate-x-2" />
               </button>
@@ -74,11 +120,13 @@ const Header = () => {
         </div>
 
         {/* Small and Medium screen */}
-        <div className="lg:hidden flex items-center justify-between gap-x-10">
-          <div className="flex items-center gap-x-12">
+        <div className="lg:hidden flex items-center justify-between gap-4 md:gap-x-10">
+          <div className="flex items-center gap-x-6 md:gap-x-12 ">
             <div className="flex items-center gap-x-3">
-              <img src={Logo} alt="logo" className="max-w-full" />
-              <span className="self-center text-2xl font-semibold whitespace-nowrap">Flowbite</span>
+              <img src={Logo} alt="logo" className="h-4 w-4 md:max-w-full" />
+              <span className="self-center text-[18px] md:text-2xl font-semibold whitespace-nowrap">
+                Flowbite
+              </span>
             </div>
 
             <div className="hidden md:flex lg:hidden items-center justify-between rounded-[8px] px-3 w-[260px] h-10 dark:bg-gray-700 text-gray-400 cursor-pointer hover:dark:bg-gray-500">
@@ -95,25 +143,32 @@ const Header = () => {
           </div>
 
           <div className="gap-4 flex items-center">
-            <div className="md:hidden dark:bg-gray-100 dark:text-gray-500 cursor-pointer hover:dark:bg-gray-600 hover:dark:text-white py-3 px-3 rounded-[6px]">
-              <RiSearch2Line className="h-5 w-5 max-w-full" />
+            <div className={`md:hidden cursor-pointer ${darkMode? "dark:bg-gray-100 dark:text-gray-500  hover:dark:bg-gray-600 hover:dark:text-white" : "bg-gray-300 text-gray-900 hover:bg-gray-200"} p-2 md:p-3 rounded-[6px]`}>
+              <RiSearch2Line className=" h-3 w-3 md:h-5 md:w-5 max-w-full" />
             </div>
 
-            <button onClick={handleToggle} className="p-2 text-xl rounded-[4px] dark:text-gray-500 dark:hover:bg-gray-700 transition-all duration-200">
-              {darkMode ? <MdDarkMode /> : <MdLightMode />}
+            <button
+              onClick={()=> setDarkMode(!darkMode)}
+              className="p-2 md:text-xl rounded-[4px] dark:text-gray-500 dark:hover:bg-gray-700 transition-all duration-200"
+            >
+              {darkMode ? <MdLightMode />:  <MdDarkMode />}
             </button>
 
             <div className="flex">
-              <button onClick={() => navigate("/login")}
-                className="py-[8px] text-[12px] md:text-[14px] md:py-[10px] px-3 md:px-5 rounded-[10px] bg-blue-700 text-white cursor-pointer flex items-center transition-all duration-200 ease-linear group hover:bg-blue-900">
+              <button
+                onClick={() => navigate("/login")}
+                className="py-[8px] text-[12px] md:text-[14px] md:py-[10px] px-3 md:px-5 rounded-[10px] bg-blue-700 text-white cursor-pointer flex items-center transition-all duration-200 ease-linear group hover:bg-blue-900"
+              >
                 Sign in
               </button>
             </div>
 
-            <div className="text-white">
-            <button onClick={() => setIsOpen(!isOpen)}
-                className="lg:hidden text-white cursor-pointer p-2 ">
-                {isOpen ? <MdMenu size={30} className="text-white"/> : <MdMenu size={30} className="text-white"/>}
+            <div className="text-gray-800  dark:text-white flex items-center">
+              <button
+                onClick={() => setIsOpen(!isOpen)}
+                className={`lg:hidden ml-1 cursor-pointer w-10 h-10 text-sm p-2.5 inline-flex items-center justify-center focus:outline-none focus:ring-4 ${darkMode ? "dark:text-gray-400 dark:hover:bg-gray-800 dark:focus:ring-gray-700  " : "text-gray-500 hover:bg-gray-100 focus:ring-gray-200"}`}
+              > 
+                <MdMenu size={30} className=""/>
               </button>
             </div>
           </div>
@@ -121,13 +176,53 @@ const Header = () => {
 
         {/* Mobile Dropdown Menu */}
         {isOpen && (
-          <div className="fixed top-15 left-0 w-full h-[300px] bg-[#070709] text-white transition-all duration-300 ease-in-out transform lg:hidden overflow-y-auto">
-
-            <ul className="flex flex-col items-start space-y-6 mt-10 px-4">
-              <li onClick={() => { navigate("/marketplace"); setIsOpen(false); }} className="text-[#9e9e9e] hover:text-white text-[16px] font-semibold cursor-pointer">MarketPlace</li>
-              <li onClick={() => { navigate("/enterprise"); setIsOpen(false); }} className="text-[#9e9e9e] hover:text-white text-[16px] font-semibold cursor-pointer">Enterprise</li>
-              <li onClick={() => { navigate("/pricing"); setIsOpen(false); }} className="text-[#9e9e9e] hover:text-white text-[16px] font-semibold cursor-pointer">Pricing</li>
-            
+          <div className={`fixed top-15 left-0 w-full h-[300px] font-medium ${darkMode ?"dark:bg-gray-900 dark:text-gray-300 ": "bg-white text-gray-900"} transition-all duration-300 ease-in-out transform lg:hidden overflow-y-auto`}>
+            <ul className="flex items-start flex-col space-y-3 md:space-y-4  mt-2 px-4 cursor-pointer">
+              <li
+                onClick={() => {
+                  navigate("/blocks");
+                  setIsOpen(false);
+                }}
+                className="hover:dark:text-blue-600"
+              >
+                Blocks
+              </li>
+              <li
+                onClick={() => navigate("/figma")}
+                className="hover:dark:text-blue-600"
+              >
+                Figma
+              </li>
+              <li
+                onClick={() => navigate("/icons")}
+                className="hover:dark:text-blue-600"
+              >
+                Icons
+              </li>
+              <li
+                onClick={() => navigate("/illustrations")}
+                className="hover:dark:text-blue-600"
+              >
+                Illustrations
+              </li>
+              <li
+                onClick={() => navigate("/blog")}
+                className="hover:dark:text-blue-700"
+              >
+                Blog
+              </li>
+              <li
+                onClick={() => navigate("/proversion")}
+                className="hover:dark:text-blue-700"
+              >
+                Pro version
+              </li>
+              <li
+                onClick={() => navigate("/pricing")}
+                className="hover:dark:text-blue-600"
+              >
+                Pricing & FAQ
+              </li>
             </ul>
           </div>
         )}
