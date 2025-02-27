943c4e13ef35b3b628f84051f76aa94143968daa
Matthew Adebayo <olumatty@gmail.com>
2025-02-27 21:13:39 +0100

    BlockSection 1.0

---
+++ src/components/BlockSection.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const BlockSection = () => {
+  return (
+    <div className='pb-8 lg:pb-26'>
+        <div className='px-6 max-w-[1440px] mx-auto lg:text-center'>
+            <div className='w-full mb-6'>
+
+            </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default BlockSection
